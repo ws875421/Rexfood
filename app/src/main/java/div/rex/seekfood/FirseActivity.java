afcfceaab91e0984f8eb7a7145d2a44f47812ff1
Rex <ws0936167890@gmail.com>
2019-04-11 00:30:49 +0800

    Initial commit

---
+++ app/src/main/java/div/rex/seekfood/FirseActivity.java
@@ -0,0 +1,67 @@
+package div.rex.seekfood;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.ViewPropertyAnimatorCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class FirseActivity extends AppCompatActivity {
+    public static final int STARTUP_DELAY = 300;
+    public static final int ANIM_ITEM_DURATION = 1000;
+    public static final int EDITTEXT_DELAY = 300;
+    public static final int VIEW_DELAY = 400;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firse);
+        ImageView logoImageView = findViewById(R.id.img_logo);
+        ViewGroup container = findViewById(R.id.container);
+
+        new CountDownTimer(4000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            public void onFinish() {
+                finish();
+            }
+        }.start();
+
+        ViewCompat.animate(logoImageView)
+                .translationY(-250)
+                .setStartDelay(STARTUP_DELAY)
+                .setDuration(ANIM_ITEM_DURATION)
+                .setInterpolator(new DecelerateInterpolator(1.2f))
+                .start();
+
+        for (int i = 0; i < container.getChildCount(); i++) {
+            View view = container.getChildAt(i);
+            ViewPropertyAnimatorCompat viewAnimator;
+
+            if (view instanceof EditText) {
+                viewAnimator = ViewCompat.animate(view)
+                        .scaleY(1).scaleX(1)
+                        .setStartDelay((EDITTEXT_DELAY * i) + 500)
+                        .setDuration(1000);
+
+            } else {
+                viewAnimator = ViewCompat.animate(view)
+                        .translationY(50).alpha(1)
+                        .setStartDelay((VIEW_DELAY * i) + 500)
+                        .setDuration(1500);
+            }
+
+            viewAnimator.setInterpolator(new DecelerateInterpolator()).start();
+        }
+
+    }
+}
---
+++ app/src/main/java/div/rex/seekfood/MainActivity.java
@@ -0,0 +1,16 @@
+package div.rex.seekfood;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(MainActivity.this, FirseActivity.class);
+        startActivity(intent);
+    }
+}
