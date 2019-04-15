307b11794472cebf4e7c5e74b19d9a7bc8b33fa9
Rex <ws0936167890@gmail.com>
2019-04-15 15:53:30 +0800

    Add  AlertDialog

--- app/src/main/java/div/rex/seekfood/FirseActivity.java
+++ app/src/main/java/div/rex/seekfood/FirseActivity.java
@@ -1,10 +1,16 @@
 package div.rex.seekfood;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPropertyAnimatorCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.DecelerateInterpolator;
@@ -29,6 +35,7 @@ public class FirseActivity extends AppCompatActivity {
         new CountDownTimer(4000, 1000) {
             public void onTick(long millisUntilFinished) {
             }
+
             public void onFinish() {
                 finish();
             }
@@ -61,5 +68,49 @@ public class FirseActivity extends AppCompatActivity {
             viewAnimator.setInterpolator(new DecelerateInterpolator()).start();
         }
 
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            AlertFragment alertFragment = new AlertFragment();
+            FragmentManager fm = getSupportFragmentManager();
+            alertFragment.show(fm, "alert");
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    public static class AlertFragment extends DialogFragment implements DialogInterface.OnClickListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog alertDialog = new AlertDialog.Builder(getActivity())
+                    //設定圖示
+                    .setIcon(R.drawable.alert)
+                    .setTitle("This is Dialog")
+                    //設定訊息內容
+                    .setMessage(R.string.message_alert)
+                    //設定確認鍵 (positive用於確認)
+                    .setPositiveButton(R.string.text_btYes, this)
+                    //設定取消鍵 (negative用於取消)
+                    .setNegativeButton(R.string.text_btNo, this)
+                    .create();
+            return alertDialog;
+        }
+
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            switch (which) {
+                case DialogInterface.BUTTON_POSITIVE:
+                    getActivity().finish();
+                    break;
+                case DialogInterface.BUTTON_NEGATIVE:
+                    dialog.cancel();
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
--- app/src/main/java/div/rex/seekfood/MainActivity.java
+++ app/src/main/java/div/rex/seekfood/MainActivity.java
@@ -1,8 +1,14 @@
 package div.rex.seekfood;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +18,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Intent intent = new Intent(MainActivity.this, FirseActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            AlertFragment alertFragment = new AlertFragment();
+            FragmentManager fm = getSupportFragmentManager();
+            alertFragment.show(fm, "alert");
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    public static class AlertFragment extends DialogFragment implements DialogInterface.OnClickListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog alertDialog = new AlertDialog.Builder(getActivity())
+                    //設定圖示
+                    .setIcon(R.drawable.alert)
+                    .setTitle("This is Dialog")
+                    //設定訊息內容
+                    .setMessage(R.string.message_alert)
+                    //設定確認鍵 (positive用於確認)
+                    .setPositiveButton(R.string.text_btYes, this)
+                    //設定取消鍵 (negative用於取消)
+                    .setNegativeButton(R.string.text_btNo, this)
+                    .create();
+            return alertDialog;
+        }
+
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            switch (which) {
+                case DialogInterface.BUTTON_POSITIVE:
+                    getActivity().finish();
+                    break;
+                case DialogInterface.BUTTON_NEGATIVE:
+                    dialog.cancel();
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
